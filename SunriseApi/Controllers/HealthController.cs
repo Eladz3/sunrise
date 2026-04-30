1036e5cc2356e2a71b184d08f488b13bd67bd81b
Glenn Smith <grsmith97@gmail.com>
2026-04-30 16:20:20 -0700

    boilerplate for backend project

---
+++ SunriseApi/Controllers/HealthController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SunriseApi.Controllers
+{
+    [ApiController]
+    [Route("health")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new
+            {
+                status = "ok",
+                timestamp = DateTime.UtcNow
+            });
+        }
+    }
+}
