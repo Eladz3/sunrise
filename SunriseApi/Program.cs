f2a23ce38ab0b3e117ff883cda0da25089ffb987
Glenn Smith <grsmith97@gmail.com>
2026-05-01 12:23:03 -0700

    configurations for local db

--- SunriseApi/Program.cs
+++ SunriseApi/Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(
-        builder.Configuration.GetConnectionString("Default")
+        builder.Configuration.GetConnectionString("SunriseSQLDatabaseConnectionString")
     ));
 
 var app = builder.Build();
